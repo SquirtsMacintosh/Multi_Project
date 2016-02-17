b1dce2cb5088943fe2b134893a3caf4f775ed8e1
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-02-16 20:54:24 -0800

    adding js functionality
    does not work currently

---
+++ Website/scripts.js
@@ -0,0 +1,5 @@
+function UserPass(){
+  var username = login.getElementsById('Username').value;
+  var password = login.getElementsById('Password').value;
+  window.alert(username + " " + password);
+}
