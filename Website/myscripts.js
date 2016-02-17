32e298ef8c9664115728dc565744aff307a1174a
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-02-16 21:33:12 -0800

    fixed button functionality
    got the button to work

---
+++ Website/myscripts.js
@@ -0,0 +1,5 @@
+function UserPass(){
+  var username = document.getElementById('Username').value;
+  var password = document.getElementById('Password').value;
+  alert(password);
+}
--- Website/scripts.js
+++
@@ -1,5 +0,0 @@
-function UserPass(){
-  var username = login.getElementsById('Username').value;
-  var password = login.getElementsById('Password').value;
-  window.alert(username + " " + password);
-}
