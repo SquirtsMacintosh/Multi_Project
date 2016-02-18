64fc5bc94e39e49bf8bd0a75601993ab23c4e734
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-02-17 21:58:33 -0800

    working out php
    working out the php for checking login information and creating an
    account into the backend db.

--- Website/myscripts.js
+++ Website/myscripts.js
@@ -2,7 +2,7 @@
 function UserPass(){
   var username = document.getElementById('Username').value;
   var password = document.getElementById('Password').value;
-  if (username.length<3 || password.length<3) {
+  if (username.length<4 || password.length<4) {
     alert("Your username and password should be at least 3 characters long");
   } else  {
     window.location.href = "main.html"; // redirects to the main page
