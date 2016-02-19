317d7e04840544fe8c6adca7e4b8b79e0abf08cb
SquirtsMacintosh <zach@comr.se>
2016-02-19 14:23:02 -0800

    minor changes

--- Website/createaccount.php
+++ Website/createaccount.php
@@ -3,6 +3,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'userinfo'); // defining all of the db variables
 define('DB_USER','root');
 define('DB_PASSWORD','root');
+//define('DB_PORT', '8889');
 
 //connecting to mysql and then to the db i've set up
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
