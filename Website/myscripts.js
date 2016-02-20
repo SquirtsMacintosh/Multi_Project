649c5a995dda65904e34f7dcd955f30858819b9d
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-02-19 18:14:40 -0800

    not much

--- Website/myscripts.js
+++ Website/myscripts.js
@@ -3,7 +3,7 @@ function UserPass(){
   var username = document.getElementById('Username').value;
   var password = document.getElementById('Password').value;
   if (username.length<4 || password.length<4) {
-    alert("Your username and password should be at least 3 characters long");
+    alert("Your username and password should be at least 4 characters long");
   } else  {
     window.location.href = "main.html"; // redirects to the main page
   }
