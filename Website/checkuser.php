fc76fd952aa06d7392f4c0b833ec6c77315bdbd6
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-02-27 19:53:53 -0800

    Now creates hashed password and logs in

--- Website/checkuser.php
+++ Website/checkuser.php
@@ -5,21 +5,19 @@ define('DB_USER','root');
 define('DB_PASSWORD','root');
 $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Failed Connecting to database!");
 
-//session_start();
-if (!empty($_POST['myUsername'])){
-  // searching for username and pw from the db
-  $query= "SELECT * FROM info WHERE username = '$_POST[myUsername]' and password
-    = '$_POST[myPassword]'";
-  $result = mysqli_query($link,$query) or die("Failed to get the query!");
+//grabs the hash from the db to verify against the password
+$sql = "SELECT password FROM info WHERE username='$_POST[myUsername]'";
+$result = mysqli_query($link, $sql);
+$col = mysqli_fetch_object($result);
+$hash = $col->password;
+//var_dump($hash);
 
-  //grabbing the array for the query
-  $row = mysqli_fetch_array($result, MYSQLI_ASSOC) or die("Failed to fetch an array of the Username!");
-  if (!empty($row['username']) AND !empty($row['password'])){
-    header("location:mainPage.php");
-  } else {
-    echo "Your username or password were inccorect!";
-  }
+//verifies that our password was correct
+if (password_verify($_POST['myPassword'], $hash)) {
+  //success
+  header("location:mainPage.php");
+} else {
+  echo "Your username or password were incorrect!";
 }
-//closes the DB connection
 //mysqli_close($link) or die("Failed to close the database connection!");
 ?>
--- Website/createaccount.php
+++ Website/createaccount.php
@@ -4,17 +4,16 @@ define('DB_NAME', 'userinfo'); // defining all of the db variables
 define('DB_USER','root');
 define('DB_PASSWORD','root');
 
-//generates a hashed password + salt
-$salt = uniqid(mt_rand(), true);
-$hash = password_hash($_POST["myPass"].$salt, PASSWORD_BCRYPT);
+//generates a hashed password --- TO-DO: find how to save salt
+$hash = password_hash($_POST["myPass"], PASSWORD_BCRYPT);
 
 //connecting to mysql and then to the db i've set up
 $link=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
   or die("Failed to connect to MySQL:");
 
 //adds created user data into database
-$sql = "INSERT INTO `info`(`username`, `password`, `email`, `salt`) VALUES
-  ('$_POST[myUser]', '$hash', '$_POST[myEmail]', '$salt')";
+$sql = "INSERT INTO `info`(`username`, `password`, `email`) VALUES
+  ('$_POST[myUser]', '$hash', '$_POST[myEmail]')";
 
 $query = mysqli_query($link,$sql) or die("Cannot retrieve data ");
 
