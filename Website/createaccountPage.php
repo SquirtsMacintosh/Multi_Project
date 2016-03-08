896af30095c89200fd015d3e8c47e7774744dd9a
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-03-08 09:46:27 -0800

    Created calendar and integrated parsley
    Will need to fix error messages

--- Website/createaccountPage.php
+++ Website/createaccountPage.php
@@ -1,26 +1,30 @@
 <html>
 <head>
   <link rel="stylesheet" type="text/css" href="webstyle.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="res/parsley.min.js"></script>
 </head>
+
 <div align="center">
   <img src="logo.png">
 </div>
 
-<div id="create_account" align="center">
-<form method="POST" action="createaccount.php">
+<div id="create_accountss" align="center">
+<form method="POST" action="createaccount.php" data-parsley-validate>
 
   <label>Create Username:</label>
-  <input class="createIn" type="text" name="myUser" required><br><br>
+  <input class="createIn" type="text" name="myUser" data-parsley-required data-parsley-length="[6,20]"><br><br>
+  <div class="error-block"></div>
 
   <label>Email:</label>
-  <input class="createIn" type="text" name="myEmail" required><br><br>
+  <input class="createIn" type="text" name="myEmail" data-parsley-required data-parsley-type="email"><br><br>
+  <div class="error-block"></div>
 
   <label>Enter Password:</label>
-  <input class="createIn" type="password" name="myPass" required><br><br>
+  <input class="createIn" type="password" name="myPass" data-parsley-required data-parsley-length="[6,30]"><br><br>
 
   <label>Retype Password:</label>
-  <input class="createIn" type="password" name="samePass" required><br><br>
+  <input class="createIn" type="password" name="samePass" data-parsley-required="[6,30]"><br><br>
 
   <input id="button" type="submit" name="submit2" value="Create">
   <button id = "button" onclick="window.location='loginPage.php'" type="submit">Back</button>
--- Website/teachertools/quizController.php
+++ Website/teachertools/quizController.php
@@ -1,51 +1,35 @@
 <html>
 <head>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <link rel="stylesheet" type="text/css" href="quiz.css">
+  <script>
+    $(function() {
+      $( "#datepicker" ).datepicker();
+    });
+  </script>
 </head>
 
 <h1>Add a New Quiz Below</h1>
 <p>Enter your quiz with the following format, currently we only support 3 questions</p>
 <br><br>
-
-<label>Enter the number of questions</label><br><br>
-<input class="quiz-ques-nums" type="text"></input>
-
+<label>Date</label>
+<input id="datepicker" class="quiz-date" type="text"></input><br><br>
    <div class="add-quiz" align="center">
       <label>Quiz Name</label>
       <input class="quiz-name" type="text"></input><br><br><br><br>
 
       <label>Question 1</label>
-      <input class="quiz-questions" type="text"></input><br><br>
+      <input id="questionOne" class="quiz-questions" type="text"></input><br><br>
       <label>a) </label>
-      <input class="quiz-options" type="text"></input><br><br>
+      <input id="optionAOne" class="quiz-options" type="text"></input><br><br>
       <label>b)</label>
-      <input class="quiz-options" type="text"></input><br><br>
+      <input id="optionBOne" class="quiz-options" type="text"></input><br><br>
       <label>c)</label>
-      <input class="quiz-options" type="text"></input><br><br>
+      <input id="optionCOne" class="quiz-options" type="text"></input><br><br>
       <label>d)</label>
-      <input class="quiz-options" type="text"></input><br><br><br><br>
-
-      <label>Question 2</label>
-      <input class="quiz-questions" type="text"></input><br><br>
-      <label>a) </label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>b)</label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>c)</label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>d)</label>
-      <input class="quiz-options" type="text"></input><br><br><br><br>
-
-      <label>Question 3</label>
-      <input class="quiz-questions" type="text"></input><br><br>
-      <label>a)</label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>b)</label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>c)</label>
-      <input class="quiz-options" type="text"></input><br><br>
-      <label>d)</label>
-      <input class="quiz-options" type="text"></input><br><br><br><br>
+      <input id="optionDOne" class="quiz-options" type="text"></input><br><br><br><br>
 
       <br>
       <input type="button" name="submit" value="SUBMIT"></input>
