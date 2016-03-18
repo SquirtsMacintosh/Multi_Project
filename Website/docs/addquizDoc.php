d3656fa9388843ce626bd00361cd735b3c6edd66
SquirtsMacintosh <zachary.paul.martin@gmail.com>
2016-03-18 15:52:52 -0700

    created basic double nav page

--- Website/docs/addquizDoc.php
+++ Website/docs/addquizDoc.php
@@ -1,22 +1,29 @@
 <html>
-  <head>
-    <link rel="stylesheet" type="style/css" href="../res/bootstrap/css/bootstrap.min.css">
-    <script src="../res/bootstrap/js/bootstrap.min.js"></script>
-  </head>
-  <div class="container-fluid">
-    <div class="row-fluid">
-      <div class="col-sm-2">
-        <ul class="nav nav-stacked">
-          <li role="presentation"><a href="../mainPage.php">Main</a></li>
-          <li role="presentation"><a href="../teachertools/quizPage.php">Add Quizzes</a></li>
-          <li role="presentation"><a href="../teachertools/gradebookPage.php">Quiz Gradebook</a></li>
-          <li role="presentation"><a href="../teachertools/courseManagerPage.php">Course Management</a></li>
-          <li role="presentation"><a href="../aboutPage.php">About</a></li>
-          <li role="presentation"><a href="../contactPage.php">Support</a></li>
-      </div>
-      <div class="col-sm-6">
-        <h1>Teacher Tools</h1>
-      </div>
-    </div>
+   <head>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+      <script src="../res/bootstrap/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" type="style/css" href="../res/bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" type="style/css" href="../style/newStyle.css">
+   </head>
+   <div class="grid">
+     <div class="row">
+       <div class="container-fluid">
+       <ul class="nav nav-tabs">
+         <li role="presentation" class="active"><a href="#">Home</a></li>
+         <li role="presentation"><a href="#">Profile</a></li>
+         <li role="presentation"><a href="#">Messages</a></li>
+       </ul>
+     </div>
+   </div>
+     <div class="row">
+         <ul class="nav nav-stacked col-lg-2">
+           <li role="presentation" align="center"><a href="../mainPage.php">Main</a></li>
+           <li role="presentation" align="center"><a href="../teachertools/quizPage.php">Add Quizzes</a></li>
+           <li role="presentation" align="center"><a href="../teachertools/gradebookPage.php">Quiz Gradebook</a></li>
+           <li role="presentation" align="center"><a href="../teachertools/courseManagerPage.php">Course Management</a></li>
+           <li role="presentation" align="center"><a href="../aboutPage.php">About</a></li>
+           <li role="presentation" align="center"><a href="../contactPage.php">Support</a></li>
+        </ul>
+     </div>
   </div>
 </html>
